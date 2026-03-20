04a9b72ed2b7b53133e503a5aa6dda2da981a616
Tania Hernandez Lira <therna24@itam.mx>
2026-03-20 13:27:57 -0600

    Añadir temas al __init__.py

---
+++ zen_focus/temas/__init__.py
@@ -0,0 +1,7 @@
+from .base import TemaBase
+from .cohete import Cohete
+from .bonsai import Bonsai
+from .edificio import Edificio
+from .bebida import Bebida
+
+__all__ = ["TemaBase", "Cohete", "Bonsai", "Edificio", "Bebida"]
