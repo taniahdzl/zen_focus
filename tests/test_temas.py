52b3be1014dbad4ac7d7e9c6f8eacd13ba6c12ea
Tania Hernandez Lira <therna24@itam.mx>
2026-03-20 14:05:57 -0600

    Añadir test_temas.py

---
+++ tests/test_temas.py
@@ -0,0 +1,19 @@
+import pytest
+from zen_focus.temas import PlantaFlor
+
+def test_evolucion_planta():
+    """Prueba que la planta suba de nivel correctamente."""
+    planta = PlantaFlor(nombre="Girasol Test")
+    assert planta.nivel_actual == 1
+    
+    planta.evolucionar()
+    assert planta.nivel_actual == 2
+
+def test_penalizacion_planta():
+    """Prueba que la planta se reinicie al nivel 1 si se penaliza."""
+    planta = PlantaFlor()
+    planta.evolucionar() # Sube a 2
+    planta.evolucionar() # Sube a 3
+    
+    planta.penalizar()
+    assert planta.nivel_actual == 1
