2cf6ec033f5ec404778df17383ff5c8639e8e839
Tania Hernandez Lira <therna24@itam.mx>
2026-04-13 13:12:32 -0600

    Actualizar __init__.py

--- zen_focus/__init__.py
+++ zen_focus/__init__.py
@@ -1,4 +1,17 @@
 from .escudo import Escudo
 from .motor import SesionZen
+from .temas import TemaBase, PlantaFlor, Cohete, Edificio, Bebida
+from .decoradores import con_progreso
+from .widgets import TemaWidget
 
-__all__ = ["Escudo", "SesionZen"]
+__all__ = [
+    "Escudo",
+    "SesionZen",
+    "TemaBase",
+    "PlantaFlor",
+    "Cohete",
+    "Edificio",
+    "Bebida",
+    "con_progreso",
+    "TemaWidget",
+]
